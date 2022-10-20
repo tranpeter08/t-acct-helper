a13e47a3799a3562079aa8fa9dbcd48e0d010de6
Peter Tran <tran.peter08@gmail.com>
2022-10-19 23:19:53 -0500

    t-acct row

---
+++ components/TrxRow.tsx
@@ -0,0 +1,57 @@
+import { FormEvent, useContext, useState } from 'react';
+import {
+  Button,
+  CloseButton,
+  Divider,
+  Grid,
+  GridItem,
+  Heading,
+  IconButton,
+  InputGroup,
+  Text,
+  Box,
+} from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+import Trx from '../classes/Trx';
+import { AppCtx } from '../pages/index';
+import { AppCtxInterface } from '../types';
+import clsx from 'clsx';
+import styles from './TAccount.module.css';
+
+type TrxRowProps = {
+  trx: Trx;
+  tAcctId: string;
+};
+
+export default function TrxRow(props: TrxRowProps) {
+  const { trx, tAcctId: id } = props;
+  const ctx = useContext(AppCtx) as AppCtxInterface;
+
+  return (
+    <GridItem colStart={1} colSpan={10}>
+      <Grid templateColumns='repeat(10, 1fr)' key={trx.id}>
+        {/* Debit */}
+        <GridItem colStart={1} colSpan={4} p={1}>
+          <Text textAlign='right'>{trx.dr === 0 ? '' : trx.dr}</Text>
+        </GridItem>
+
+        {/* Credit */}
+        <GridItem colStart={5} colSpan={4} borderLeft='solid black 1px' p={1}>
+          <Text textAlign='right'>{trx.cr === 0 ? '' : trx.cr}</Text>
+        </GridItem>
+
+        {/* Delete Button */}
+        <GridItem colStart={10} colSpan={1}>
+          <Box></Box>
+          <IconButton
+            aria-label='Delete Transaction'
+            icon={<DeleteIcon />}
+            size='xs'
+            onClick={() => ctx.removeTrx(id, trx.id)}
+            title='Delete Transaction'
+          />
+        </GridItem>
+      </Grid>
+    </GridItem>
+  );
+}
