665ec09b5d6435630a06a7cd8651481f3e067f3a
Peter Tran <tran.peter08@gmail.com>
2022-10-24 14:38:05 -0500

    added validation for TAccountData

--- classes/TAccountData.ts
+++ classes/TAccountData.ts
@@ -3,7 +3,27 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default class TAccountData {
   public id: string = uuidv4();
+  public title: string = '';
   public trxs: Trx[] = [];
 
-  constructor(public title: string) {}
+  constructor(title: string, trxs: Trx[] = []) {
+    TAccountData.validateTitle(title);
+    this.title = title;
+
+    for (const trx of trxs) {
+      TAccountData.validateTrx(trx);
+      this.trxs.push(trx);
+    }
+  }
+
+  static validateTrx(trx: any) {
+    if (!('dr' in trx && 'cr' in trx)) {
+      throw new Error('missing "credit" and "debit" properties');
+    }
+  }
+
+  static validateTitle(title: any) {
+    if (typeof title !== 'string')
+      throw new Error('title needs to be a string');
+  }
 }
